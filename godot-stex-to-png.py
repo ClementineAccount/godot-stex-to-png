d355f80a20d6576d036eee5b51622e0271c5db5b
clem <clementinepersonal@gmail.com>
2021-07-18 22:24:30 +0800

    add the actual py file

---
+++ godot-stex-to-png.py
@@ -0,0 +1,27 @@
+# godot-stex-to-png.py
+# https://github.com/ClementineAccount/godot-stex-to-png
+
+
+import sys
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+    inputFile = open(filename, 'rb')
+    outputFile = open('output.png', 'wb')
+
+    # 32 bits is based off the .stex header which are primmed for png
+    inputFile.seek(32)
+
+    while 1:
+        #read by char
+        char = inputFile.read(1)
+        outputFile.write(char)
+        
+        if not char:
+            break
+
+    inputFile.close()
+    outputFile.close()
+else:
+    print("Usage: python godot-stex-to-png.py input.stex")
+
+
